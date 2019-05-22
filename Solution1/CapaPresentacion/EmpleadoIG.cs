51a91bf995b231251b80774fbdd229d3ff5a646f
JavierGH2019 <47840404+JavierGH2019@users.noreply.github.com>
2019-05-21 20:00:38 -0600

    Add files via upload

---
+++ Solution1/CapaPresentacion/EmpleadoIG.cs
@@ -0,0 +1,66 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using capaNegocio;
+using CapaPresentacion;
+
+namespace CapaPresentacion
+{
+    public partial class EmpleadoIG : Form
+    {
+        MEmpleado mEmpleado = new MEmpleado();
+        public EmpleadoIG()
+        {
+            InitializeComponent();
+            cargar();
+        }
+        public void limpiar()
+        {
+            dataGridView1.DataSource = null;
+
+            dataGridView1.Refresh();
+        }
+        public void cargar()
+        {
+            mEmpleado.mostrarEmpleado();
+             
+            dataGridView1.DataSource = mEmpleado.tabla;
+
+        }
+        private void label6_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            mEmpleado.Emp_no = textBox1.Text;
+            mEmpleado.Apellido = textBox2.Text;
+            mEmpleado.Oficio = textBox3.Text;
+            mEmpleado.Direccion = Convert.ToInt16(textBox4.Text);
+            mEmpleado.Fecha_alta = textBox5.Text;
+            mEmpleado.salario = Convert.ToDouble(textBox6.Text);
+            mEmpleado.Comision =Convert.ToDouble(textBox7.Text);
+            mEmpleado.Dept_no = textBox8.Text;
+
+
+
+            mEmpleado.insertarEmpleado();
+            limpiar();
+            cargar();
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            mEmpleado.Emp_no = textBox1.Text;
+            mEmpleado.eliminarEmpleado();
+            cargar();
+        }
+    }
+}
---
+++ Solution1/capaNegocio/MEmpleado.cs
@@ -0,0 +1,133 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using CapaDatos;
+using System.Data;
+
+namespace capaNegocio
+{
+   public class MEmpleado
+    {
+        bdConex test = new bdConex();
+
+        public DataTable tabla = new DataTable();
+
+        public string emp_no, apellido, oficio, fecha_alta, dept_no;
+        public double salario, comision;
+           public int direccion;
+
+        public string Oficio
+        {
+            get { return oficio; }
+            set { oficio = value; }
+        }
+
+        public string Dept_no
+        {
+            get { return dept_no; }
+            set { dept_no = value; }
+        }
+
+        public double Comision
+        {
+            get { return comision; }
+            set { comision = value; }
+        }
+
+        public double Salario
+        {
+            get { return salario; }
+            set { salario = value; }
+        }
+
+        public string Fecha_alta
+        {
+            get { return fecha_alta; }
+            set { fecha_alta = value; }
+        }
+
+        public int Direccion
+        {
+            get { return direccion; }
+            set { direccion = value; }
+        }
+
+        public string Apellido
+        {
+            get { return apellido; }
+            set { apellido = value; }
+        }
+
+        public string Emp_no
+        {
+            get { return emp_no; }
+            set { emp_no = value; }
+        }
+
+
+        public void insertarEmpleado()
+        {
+            try
+            {
+
+                String insert = "INSERT INTO Emp( emp_no, apellido, oficio, dir, Fecha_Alt, salario, comision, dept_no) VALUES('" + emp_no + "','" + apellido + "','" + oficio + "','" + direccion + "','" + fecha_alta + "','" + salario + "','" + comision + "','" + dept_no + "')";
+                test.Conectar();
+                test.consultaSQL(insert);
+                test.Desconectar();
+            }
+            catch
+            {
+
+            }
+        }
+
+
+        public void actualizarEmpleado()
+        {
+            try
+            {
+                String update = " UPDATE [dbo].[Emp] SET [Emp_No] = '" + emp_no + "' ,[Apellido] = '" + apellido + "' ,[Oficio] = '" + oficio + "',[Dir] = '" + direccion + "',[Fecha_Alt] = '" + fecha_alta + "' ,[Salario] = '" + salario + "',[Comision] = '" + comision + "' ,[Dept_No] = '" + dept_no + "'  where emp_no='" + emp_no + "'";
+                
+                test.Conectar();
+                test.consultaSQL(update);
+                test.Desconectar();
+            }
+            catch
+            {
+
+            }
+        }
+
+        public void eliminarEmpleado()
+        {
+            try
+            {
+                String delete = "delete  from Emp where emp_no='" + emp_no + "'";
+                test.Conectar();
+                test.consultaSQL(delete);
+                test.Desconectar();
+            }
+            catch
+            {
+
+            }
+        }
+        public void mostrarEmpleado()
+        {
+            try
+            {
+                String consulta = "select * from Emp";
+                test.Conectar();
+                test.mostrarSQL(consulta);
+                test.da.Fill(tabla);
+                test.Desconectar();
+            }
+            catch
+            {
+
+            }
+        }
+    }
+}
